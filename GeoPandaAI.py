af51c231dfbd8bea985dd4d355b7e2d940120354
Alok R Mishra <alokranjan660@gmail.com>
2020-07-06 01:00:17 +0400

    Add files via upload

---
+++ GeoPandaAI.py
@@ -0,0 +1,81 @@
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+    help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+    help="max buffer size")
+args = vars(ap.parse_args())
+ 
+# define the lower and upper boundaries of the colors in the HSV color space
+lower = {'red':(166, 84, 141), 'green':(66, 122, 129), 'blue':(97, 100, 117), 'yellow':(23, 59, 119), 'orange':(0, 50, 80)} #assign new item lower['blue'] = (93, 10, 0)
+upper = {'red':(186,255,255), 'green':(86,255,255), 'blue':(117,255,255), 'yellow':(54,255,255), 'orange':(20,255,255)}
+
+# define standard colors for circle around the object
+colors = {'red':(0,0,255), 'green':(0,255,0), 'blue':(255,0,0), 'yellow':(0, 255, 217), 'orange':(0,140,255)}
+
+if not args.get("video", False):
+    camera = cv2.VideoCapture(0)
+    
+ 
+# otherwise, grab a reference to the video file
+else:
+    camera = cv2.VideoCapture(args["video"])
+# keep looping
+while True:
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+   
+    if args.get("video") and not grabbed:
+        break
+ 
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=1200)
+
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    #for each color in dictionary check object in frame
+    for key, value in upper.items():
+      
+        kernel = np.ones((9,9),np.uint8)
+        mask = cv2.inRange(hsv, lower[key], upper[key])
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+                
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
+        
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+           
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        
+            # only proceed if the radius meets a minimum size. Correct this value for your obect's size
+            if radius > 0.5:
+               
+                cv2.circle(frame, (int(x), int(y)), int(radius), colors[key], 2)
+                cv2.putText(frame,key + " ball", (int(x-radius),int(y-radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,colors[key],2)
+
+         
+    cv2.imshow("Geo-Colour Frame", frame)
+    
+    key = cv2.waitKey(1) & 0xFF
+   
+    if key == ord("q"):
+        break
+ 
+
+camera.release()
+cv2.destroyAllWindows()
